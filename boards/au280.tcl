00a46e51ef0188d4b8f6903357b9d4f70d63463e
Li Yunfan <dcsyuli@nus.edu.sg>
2022-11-07 09:55:51 +0000

    Fixed au280 ver 1.3 and gt issues

--- boards/au280.tcl
+++ boards/au280.tcl
@@ -10,15 +10,22 @@ set board_part xilinx.com:au280:part0:$board_rev
 proc cmac_set_property {cmac_ip port_id} {
     global board_rev
     if {$port_id == 0} {
+# Workaround for board files ver 1.3
+# Absence of board params requires CAUI and LANES to be set first
         if {$board_rev != 1.3} {
             set_property -dict {
                 CONFIG.ETHERNET_BOARD_INTERFACE {qsfp0_4x}
                 CONFIG.DIFFCLK_BOARD_INTERFACE {qsfp0_156mhz}
             } [get_ips $cmac_ip]
+        } else {
+            set_property -dict {
+                CONFIG.CMAC_CAUI4_MODE {1}
+                CONFIG.NUM_LANES {4x25}
+            } [get_ips $cmac_ip]
         }
         set_property -dict {
             CONFIG.CMAC_CORE_SELECT {CMACE4_X0Y6}
-            CONFIG.GT_GROUP_SELECT {X1Y40~X1Y43}
+            CONFIG.GT_GROUP_SELECT {X0Y40~X0Y43}
             CONFIG.LANE1_GT_LOC {X0Y40}
             CONFIG.LANE2_GT_LOC {X0Y41}
             CONFIG.LANE3_GT_LOC {X0Y42}
@@ -29,6 +36,11 @@ proc cmac_set_property {cmac_ip port_id} {
             set_property -dict {
                 CONFIG.ETHERNET_BOARD_INTERFACE {qsfp1_4x}
                 CONFIG.DIFFCLK_BOARD_INTERFACE {qsfp1_156mhz}
+            } [get_ips $cmac_ip]
+        } else {
+            set_property -dict {
+                CONFIG.CMAC_CAUI4_MODE {1}
+                CONFIG.NUM_LANES {4x25}
             } [get_ips $cmac_ip]
         }
         set_property -dict {
